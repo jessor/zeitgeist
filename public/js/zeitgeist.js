43901aa8349a70293fd5b132c531705238d2f8ab
jessor <daniel@enthusiasm.cc>
2011-07-05 02:58:45 +0200

    make taglist in index view shorter but expandable

--- public/js/zeitgeist.js
+++ public/js/zeitgeist.js
@@ -1,18 +1,25 @@
 jQuery(function(){
 
     $('body').noisy({
-        'intensity' : 1, 
-        'size' : '200', 
-        'opacity' : 0.034, 
-        'fallback' : '', 
-        'monochrome' : false
+        'intensity':    1, 
+        'size':         '200', 
+        'opacity':      0.034, 
+        'fallback':     '', 
+        'monochrome':   false
     });
 
     $().piroBox({
-        my_speed: 100, //animation speed
-        bg_alpha: 0.8, //background opacity
-        slideShow : 'false', // true == slideshow on, false == slideshow off
-        close_all : '.piro_close' // add class .piro_overlay(with comma)if you want overlay click close piroBox
+        my_speed:   100, //animation speed
+        bg_alpha:   0.8, //background opacity
+        slideShow:  'false', // true == slideshow on, false == slideshow off
+        close_all:  '.piro_close' // add class .piro_overlay(with comma)if you want overlay click close piroBox
+    });
+
+    $('.taglist').expander({
+        collapseTimer:  5000,
+        slicePoint:     200,
+        expandText:     '&raquo; more',
+        userCollapse:   false
     });
 
     $('form.tag').submit(function() {
