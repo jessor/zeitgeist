6f8a9518234933ea94edf9c30cb774bddae090e7
jessor <daniel@enthusiasm.cc>
2011-07-05 02:43:37 +0200

    moved jquery stuff to a file

---
+++ public/js/zeitgeist.js
@@ -0,0 +1,30 @@
+jQuery(function(){
+
+    $('body').noisy({
+        'intensity' : 1, 
+        'size' : '200', 
+        'opacity' : 0.034, 
+        'fallback' : '', 
+        'monochrome' : false
+    });
+
+    $().piroBox({
+        my_speed: 100, //animation speed
+        bg_alpha: 0.8, //background opacity
+        slideShow : 'false', // true == slideshow on, false == slideshow off
+        close_all : '.piro_close' // add class .piro_overlay(with comma)if you want overlay click close piroBox
+    });
+
+    $('form.tag').submit(function() {
+        var Id = $(this).attr("id")
+        var tagtarget = '#' + Id.replace(/formfor/, 'tagsfor')
+        var options = {
+            target: tagtarget,
+            resetForm: true
+        };
+
+        $(this).ajaxSubmit(options);
+        return false;
+    });
+
+});
