a0acb2d379cb08612a803c0d0baf570dc07932e8
Matthias -apoc- Hecker <apoc@sixserv.org>
2011-08-12 11:07:32 +0200

    the real deal xkcd bugfix

--- lib/remote/plugins/xkcd.rb
+++ lib/remote/plugins/xkcd.rb
@@ -2,9 +2,9 @@ class Xkcd < Plugin
   TYPE = 'image'
   PATTERN = %r{xkcd\.(com|org)/\d*}
   def url
-    search_one '/html/body/div/div[2]/div/div[2]/div/div/img/@src'
+    search_one('/html/body/div/div[2]/div/div[2]/div/div/img/@src') || super
   end
   def title
-    search_one '.s h1/text()'
+    search_one('.s h1/text()') || super
   end
 end
