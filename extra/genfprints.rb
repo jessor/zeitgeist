930963ba74adf8360c97d1bbde204b62ccdfb0a8
Matthias H <apoc@sixserv.org>
2012-04-13 23:29:45 +0200

    fix some bugs in the fingerprint generation script

--- extra/genfprints.rb
+++ extra/genfprints.rb
@@ -6,6 +6,7 @@
 
 puts 'Usage: %s [-f]' % $0
 puts ' -f    override existing hashes'
+override=false
 if ARGV.include? '-f'
   override = true
 end
@@ -15,16 +16,21 @@ require_relative '../zeitgeist.rb'
 
 params = {:type => 'image'}
 if not override
-  params.merge(:fingerprint.not => nil)
+  params.merge!(:conditions => ['fingerprint IS NULL'])
 end
+puts params.inspect
 
 total = Item.count(params)
 start = Time.now
 current = 1
 puts 'generating %d fingerprints' % total
 Item.all(params).each do |item|
+  begin
   fingerprint = item.generate_fingerprint
   item.update(:fingerprint => fingerprint)
+  rescue
+      puts "err: #{$!}"
+  end
   eta = (((Time.now - start) / current) * (total - current)) / 60
   print "calculating %05d/%05d [ETA:%dmin]\r" % [current, total, eta.to_i]
   current+=1
