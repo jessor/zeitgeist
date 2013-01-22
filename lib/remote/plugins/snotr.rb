42647aec4a3c571ce50fc16018d076d43b905089
Matthias H <apoc@sixserv.org>
2013-01-22 15:56:49 +0100

    new snotr.com plugin

---
+++ lib/remote/plugins/snotr.rb
@@ -0,0 +1,32 @@
+
+class Snotr < Plugin
+  TYPE = 'video'
+  PATTERN = %r{http[s]?://(www\.)?(snotr\.com/video/\d+)}
+
+  def filename
+    return self.title.gsub(/[^a-zA-Z0-9_\-\.]/, '')
+  end
+
+  def url # return url to preview image
+    search_one('link[@rel="image_src"]/@href') || super
+  end
+
+  def title
+    og_search 'title'
+  end
+
+  def embed
+    <<snotr
+    <iframe src="http://www.snotr.com/embed/#{video_id}"
+      width="400" height="330" frameborder="0"></iframe>
+snotr
+  end
+
+  private
+
+  def video_id
+    @orig_url.match %r{video/(\d+)}
+    return $1
+  end
+end
+
