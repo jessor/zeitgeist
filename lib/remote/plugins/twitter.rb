f8553798cfe239fc95a2647ecc329f3d055ceec6
Matthias H <apoc@sixserv.org>
2013-09-13 16:19:59 +0200

    twitter closed open api, scrape it

--- lib/remote/plugins/twitter.rb
+++ lib/remote/plugins/twitter.rb
@@ -4,20 +4,11 @@ class Twitter < Plugin
   PATTERN = %r{http[s]?://(www)?twitter\.com/.*status/\d+}
 
   def url
-    @orig_url.match %r{status/(\d+)}
-    id = $1
-    @item = json('http://api.twitter.com/1/statuses/show.json?include_entities=true&contributor_details=true&id=%d' % id)
-
-    media = @item['entities']['media'].first
-    if media['type'] == 'photo'
-      media['media_url']
-    else
-      nil
-    end
+    search_one('.media-slideshow-image/@src')
   end
 
   def title
-    @item['text']
+    search_one('p.tweet-text').map(&:content)
   end
 end
 
