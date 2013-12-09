eaf7a22c5b4f3cab327739affd36f5eb7b586253
Matthias Hecker <apoc@sixserv.org>
2013-12-09 15:31:24 +0000

    deactivate hover-effects during scrolling

--- public/js/zeitgeist.js
+++ public/js/zeitgeist.js
@@ -432,5 +432,18 @@ $(document).ready(function() {
         location.href = '/list/dimensions/' + ratio;
     });
 });
-    
+var body = document.body,
+    timer;
+
+window.addEventListener('scroll', function() {
+  clearTimeout(timer);
+  if(!body.classList.contains('disable-hover')) {
+    body.classList.add('disable-hover')
+  }
+
+  timer = setTimeout(function(){
+    body.classList.remove('disable-hover')
+  },500);
+}, false);
+
 
